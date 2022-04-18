c4b57503e15da22fbfc4b3e8298278c0a35bfb7d
alex-sandra22 <91840113+alex-sandra22@users.noreply.github.com>
2022-04-18 16:41:37 +0300

    Create script.js

---
+++ script.js
@@ -0,0 +1,7 @@
+let group = document.querySelectorAll(".element__group");
+function hearts(){
+    console.log("привет ");
+    renderAdded();
+}
+group.addEventListener('click',hearts);
+ renderAdded();

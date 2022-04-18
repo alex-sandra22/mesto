19585765c7ac46fd25f09736d3add874405dcec0
alex-sandra22 <91840113+alex-sandra22@users.noreply.github.com>
2022-04-18 20:43:57 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,7 +1,32 @@
 let group = document.querySelectorAll(".element__group");
 function hearts(){
-    console.log("привет ");
-    renderAdded();
+    group.classList.add("add");
 }
-group.addEventListener('click',hearts);
- renderAdded();
+group.addEventListener("click", hearts);
+let form = document.querySelector(".form");
+function clickTwo(){
+    form.classList.add("add");
+}
+form.addEventListener("click", clickTwo);
+
+let container= form.querySelector(".form__contaner");
+let submit = form.querySelector(".formm__button");
+function profile() {
+    let name = form.querySelector(".form__name");
+    let profession = form.querySelector(".form__profession");
+  
+    container.insertAdjacentHTML('beforeend', `
+          <div class="form__container">
+                <h2 class="form__title">Редактировать профиль</h2>
+                <h3 class="form__name" >${name.value}</h3>
+                <p class="form__profession" >${profession.value}</h4>
+                <button class="form__button" type="submit">Сохранить</button>
+            </div>
+    `);
+    name.value='';
+    profession.value='';
+    renderAdded();
+  }  
+  submit.addEventListener("click", profile);
+
+

2408f7ea1f865c2369873f2cca2a5a3f4a737f92
alex-sandra22 <91840113+alex-sandra22@users.noreply.github.com>
2022-04-25 00:47:44 +0300

    Create index.js

---
+++ index.js
@@ -0,0 +1,29 @@
+let popup = document.querySelector(".popup");
+let openpopup = document.querySelector(".open-popup");
+openpopup.addEventListener("click",function(){
+    popup.classList.add("popup_opened");
+})
+let form = document.querySelector(".form");
+let button = document.querySelector(".form__button");
+button.addEventListener("submit", function(){
+    let name = document.querySelector(".form__name");
+    let profession = document.querySelector(".form__profession");
+    form.textContent('beforeend', `
+    <div class="profile__info">
+          <div class="profilee__line">
+              <h1 class="profile__name">${name.value}<button src='./photos/Vector 2.0.png' class="profile__edit_button" >
+                  <img class="vector" src="./photos/Vector 2.0.png" >
+              </button>
+              </h1>
+          </div>
+          <h3 class="profile__inf">${profession.value}</h3>
+        </div>
+`);
+name.value='';
+profession.value='';
+})
+let like = document.querySelectorAll(".element__group");
+let groupadd = document.querySelectorAll('.element__group-add');
+like.addEventListener("click", function(){
+    like.classList.toggle("element__group-add");
+})

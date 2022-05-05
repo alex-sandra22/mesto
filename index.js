c2a5fcae7be2511a2df641558365e777d46b4d3b
alex-sandra22 <91840113+alex-sandra22@users.noreply.github.com>
2022-05-06 00:26:04 +0300

    Update index.js

--- index.js
+++ index.js
@@ -1,30 +1,26 @@
 let popup = document.querySelector(".popup");
 let openpopup = document.querySelector(".open-popup");
+let like = document.querySelectorAll(".element__group");
+let groupadd = document.querySelectorAll(".element__group-add");
+like.addEventListener("click", function(){
+    like.classList.toggle("element__group-add");
+})
 openpopup.addEventListener("click",function(){
     popup.classList.add("popup_opened");
 })
 let form = document.querySelector(".form");
-let button = document.querySelector(".popup__button");
-button.addEventListener("submit", function(){
+let submit = document.querySelector(".popup__button");
+submit.addEventListener("click", function(){
     let name = document.querySelector(".popup__name");
     let profession = document.querySelector(".popup__profession");
-    form.textContent('beforeend', `
-    <div class="profile__info">
-          <div class="profilee__line">
-              <h1 class="profile__name">${name.value}<button src='./photos/Vector 2.0.png' class="profile__edit_button" >
-                  <img class="vector" src="./photos/Vector 2.0.png" >
-              </button>
-              </h1>
-          </div>
-          <h3 class="profile__inf">${profession.value}</h3>
-        </div>
-`);
-name.value='';
-profession.value='';
+    let profileName = document.querySelector(".profile__name");
+    let profilepProfession = document.querySelector(".profile__profession");
+    name.value="";
+    profession.value="";
+    profileName.textContent=name.value;
+    profilepProfession.textContent=profession.value;
+
 })
-let like = document.querySelectorAll(".element__group");
-let groupadd = document.querySelectorAll(".element__group-add");
-like.addEventListener("click", function(){
-    console.log("CONTROL");
-    like.classList.toggle("element__group-add");
-})
+document.body.append("submit");
+
+

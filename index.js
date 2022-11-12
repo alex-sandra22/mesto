f07019cf01aa7e7306aeb4abba6c973588166b08
alex-sandra22 <91840113+alex-sandra22@users.noreply.github.com>
2022-11-12 11:51:18 +0300

    Update index.js

--- index.js
+++ index.js
@@ -1,32 +1,32 @@
-let popup = document.querySelector(".popup"); 
-let openpopup = document.querySelector(".open-popup"); 
-let ProfileProf = document.querySelector(".profile__profession");
-let profileName = document.querySelector(".profile__name");
-let PopupName = document.querySelector(".popup__name");
-let popupProfession = document.querySelector(".popup__profession");
-let popupSubmit = document.querySelector(".popup__button");
-let body =document.querySelector(".page");
+const popup = document.querySelector(".popup"); 
+const openpopup = document.querySelector(".open-popup"); 
+const ProfileProf = document.querySelector(".profile__profession");
+const profileName = document.querySelector(".profile__name");
+const PopupName = document.querySelector(".popup__name");
+const popupProfession = document.querySelector(".popup__profession");
+const popupSubmit = document.querySelector(".popup__button");
+const body =document.querySelector(".page");
 openpopup.addEventListener("click",function(){ 
     popup.classList.add("popup__opened"); 
-})
-popupSubmit.addEventListener("click", function(closePopup){
+});
+popupSubmit.addEventListener("click", function(){
     popup.classList.remove(".popup__opened");
-})
+});
 function formSubmitHandler (evt) {
     evt.preventDefault(); 
     openpopup.addEventListener("click",function(){
         PopupName.value = profileName.textContent;
         popupProfession.value = ProfileProf.textContent;
-    })
+    });
     popupSubmit.addEventListener("click",()=>{
         profileName.value = PopupName.textContent;
         ProfileProf.value = popupProfession.textContent;
-    })
+    });
 }
 document.body.append("profileName");
 document.body.append("ProfileProf");
 let like = document.querySelectorAll(".element__group");
 like.addEventListener("click",()=>{
     like.classList.toggle("element__group-add");
-})
+});
 popup.addEventListener('submit', formSubmitHandler);

db44f1f06739579a4cf5a333066c22107b704880
alex-sandra22 <91840113+alex-sandra22@users.noreply.github.com>
2022-11-16 19:12:48 +0300

    Update index.js

--- scripts/index.js
+++ scripts/index.js
@@ -1,26 +1,32 @@
-let Name = document.querySelector(".profile__name");
-let Profile = document.querySelector(".profile__profession");
-let NamePopup = document.querySelector(".popup__name");
-let ProfilePopup = document.querySelector(".popup__profession");
-let Popup = document.querySelector(".popup");
-let OpenPopup = document.querySelector(".popup_opened");
-let body = document.querySelector(".page");
+let popup = document.querySelector(".popup"); 
+let openpopup = document.querySelector(".popup_opened"); 
+let ProfileProf = document.querySelector(".profile__profession");
+let profileName = document.querySelector(".profile__name");
+let PopupName = document.querySelector(".popup__name");
+let popupProfession = document.querySelector(".popup__profession");
+let popupSubmit = document.querySelector(".popup__button");
+let body =document.querySelector(".page");
 let EdiButton = document.querySelector(".profile__edit-button");
-let PopupSubmit = document.querySelector(".popup__button");
-
+let closePopup = document.querySelector(".popup__close");
+EdiButton.addEventListener("click",function(){ 
+    popup.classList.add("popup_opened");
+    PopupName.value = profileName.textContent;
+    popupProfession.value = ProfileProf.textContent;
+});
+closePopup.addEventListener("click", function(closePopup){
+    popup.classList.remove("popup_opened");
+});
 function formSubmitHandler (evt) {
     evt.preventDefault(); 
     EdiButton.addEventListener("click",function(){
-        Popup.classList.remove(".popup");
-        Popup.classList.add("popup_opened"); 
         PopupName.value = profileName.textContent;
         popupProfession.value = ProfileProf.textContent;
-    })
-    PopupSubmit.addEventListener("click",()=>{
-        Popup.classList.remove(".popup_opened");
-        Popup.classList.add(".popup");
-        profileName.value = PopupName.textContent;
-        ProfileProf.value = popupProfession.textContent;
-    })
-}
-formSubmitHandler();
+    });
+    popupSubmit.addEventListener("click",function(){
+        profileName.textContent = PopupName.value;
+        ProfileProf.textContent = popupProfession.value;
+        closePopup();
+    });
+};
+
+popup.addEventListener('submit', formSubmitHandler);

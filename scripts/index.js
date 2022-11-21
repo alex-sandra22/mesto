000a4ff51acf0bc02cadf87283a26a4c4e24b3c0
alex-sandra22 <91840113+alex-sandra22@users.noreply.github.com>
2022-11-21 18:16:21 +0300

    Update index.js

--- scripts/index.js
+++ scripts/index.js
@@ -1,34 +1,29 @@
 let popup = document.querySelector(".popup"); 
 let openpopup = document.querySelector(".popup_opened"); 
-let ProfileProf = document.querySelector(".profile__profession");
+let profileProf = document.querySelector(".profile__profession");
 let profileName = document.querySelector(".profile__name");
-let PopupName = document.querySelector(".popup__name");
-let popupProfession = document.querySelector(".popup__profession");
+let popupName = document.querySelector(".popup__input_type_name");
+let popupProfession = document.querySelector(".popup__input_type_profession");
 let popupSubmit = document.querySelector(".popup__button");
 let body =document.querySelector(".page");
-let EdiButton = document.querySelector(".profile__edit-button");
+let ediButton = document.querySelector(".profile__edit-button");
 let closePopup = document.querySelector(".popup__close");
-EdiButton.addEventListener("click",function(){ 
-    popup.classList.remove("popup");
+function openPopup(){
     popup.classList.add("popup_opened");
-    PopupName.value = profileName.textContent;
-    popupProfession.value = ProfileProf.textContent;
-});
-closePopup.addEventListener("click", function(closePopup){
-    popup.classList.remove("popup_opened");
-    popup.classList.add("popup");
-});
-function formSubmitHandler (evt) {
-    evt.preventDefault(); 
-    EdiButton.addEventListener("click",function(){
-        PopupName.value = profileName.textContent;
-        popupProfession.value = ProfileProf.textContent;
-    });
-    popupSubmit.addEventListener("click",function(){
-        profileName.textContent = PopupName.value;
-        ProfileProf.textContent = popupProfession.value;
-        closePopup();
-    });
+    popupName.value = profileName.textContent;
+    popupProfession.value = profileProf.textContent;
 };
+ function closepopup(){
+    popup.classList.remove("popup_opened");
+};
+
+function formSubmitHandler(evt) {
+    evt.preventDefault(); 
+    profileName.textContent = popupName.value;
+    profileProf.textContent = popupProfession.value;
+    closepopup();
+};
+ediButton.addEventListener("click",openPopup);
+popupSubmit.addEventListener("click",formSubmitHandler);
 
 popup.addEventListener('submit', formSubmitHandler);
